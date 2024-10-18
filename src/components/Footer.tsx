f0385fc07c2c4e2d41859733406b45ea59ddc985
ecc97 <carmonaedwin1123@gmail.com>
2024-10-17 21:51:36 -0500

    chore: setting footer and hero section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,10 @@ export default function Footer() {
                 <Link href="https://github.com/ecc97" target="_blank" rel="noopener noreferrer">
                     <img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="GitHub profile" />
                 </Link>
-                <Link href="https://www.linkedin.com/in/esteban-ca%C3%B1iza-carrillo-7507b41b8/" target="_blank" rel="noopener noreferrer">
+                <Link href="https://www.linkedin.com/in/edwin-carmona-c/" target="_blank" rel="noopener noreferrer">
                     <img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn profile" />
                 </Link>
-                <Link href="https://twitter.com/ecc97" target="_blank" rel="noopener noreferrer">
+                <Link href="https://x.com/ecckyo97" target="_blank" rel="noopener noreferrer">
                     <img src="https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white" alt="Twitter profile" />
                 </Link>
             </div>
--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 import { FaGithub, FaLinkedin } from 'react-icons/fa'
 import { FaXTwitter } from 'react-icons/fa6'
 import styles from './hero.module.css'
@@ -10,16 +11,22 @@ export default function HeroSection() {
         <section className={`flex flex-col container-fluid justify-center items-center py-36 gap-2`}>
             <h1 className='text-5xl'>Edwin Carmona</h1>
             <h2 className='text-2xl'>Frontend Developer</h2>
-            <img src='https://via.placeholder.com/200x200' alt='Profile Pic' className='mt-8 rounded-full' />
+            <Image src='https://w0.peakpx.com/wallpaper/165/445/HD-wallpaper-neon-mask-hacker-hackers-lonely-hacker-neon-mask-thumbnail.jpg' alt='Avatar Picture' className={`mt-8 rounded-full ${styles.imgAvatar}`} width={200} height={200} />
             <div className='flex gap-3'>
                 <p className={`${styles.heroIconSocialNetwork} w-12 h-12`}>
-                    <FaGithub className='w-full h-full'/>
+                    <a href='https://github.com/ecc97' target='_blank'>
+                        <FaGithub className='w-full h-full'/>
+                    </a>
                 </p>
                 <p className={`${styles.heroIconSocialNetwork} w-12 h-12`}>
-                    <FaLinkedin className='w-full h-full'/>
+                    <Link href='https://www.linkedin.com/in/edwin-carmona-c/' target='_blank'>
+                        <FaLinkedin className='w-full h-full'/>
+                    </Link>
                 </p>
                 <p className={`${styles.heroIconSocialNetwork} w-12 h-12`}>
-                    <FaXTwitter className='w-full h-full'/>
+                    <Link href='https://x.com/ecckyo97' target='_blank'>
+                        <FaXTwitter className='w-full h-full'/>
+                    </Link>
                 </p>
             </div>
             <Link href='/about' className='mt-8 text-xl font-bold rounded-xl p-3 bg-sky-700 hover:bg-sky-500 hover:shadow-lg hover:shadow-cyan-500'>
