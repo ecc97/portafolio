53bd6e07d49de3f07bab9646fa6cfe696e1ba7aa
ecc97 <carmonaedwin1123@gmail.com>
2024-10-17 21:43:58 -0500

    feat: nextconfig remote images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            { protocol: 'https', hostname: 'w0.peakpx.com' },
+            { protocol: 'https', hostname: 'res.cloudinary.com' },
+        ]
+    }
+};
 
 export default nextConfig;
